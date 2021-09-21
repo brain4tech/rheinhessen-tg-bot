dd1b592798fcf56a2834889607307d820596d131
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-09-21 15:33:27 +0200

    new function-file (empty)

---
+++ src/lib/onNewChatMember.py
@@ -0,0 +1,10 @@
+# function to happen when a new group participant joins for the first time
+
+# store new user id
+# remove user rights
+# welcome-message + inline-keyboard with link to bot
+# timer with 30 seconds
+
+# if user presses button edit message to <insert here> and remove internal timer
+# if other user clicks on button: callbackquery with information
+# if user does not click on button and timer is 0 kick user from group
