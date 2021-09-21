b915842c93acd2324d42e79d3d269879198a19ea
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-09-21 20:24:39 +0200

    new users get kicked within a time frame

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # main script
 
-from time import sleep
+from time import sleep, time
 
 import path_setup as setup
 
@@ -11,22 +11,34 @@ from classes.user_id_list import UserIdList
 
 if __name__ == '__main__':
 
+    def newChatMember(chat, user, time, userlist):
+        print (f"New chat member {user} in {chat} at {time}")
+        userlist.register(chat, user, time)
+
     setup.enable()
 
     with open('bot_credentials/chat_id.txt', 'r') as file:
         chat_id = file.read()
 
     with open('bot_credentials/token.txt', 'r') as file:
-        bot = TelegramBot(file.read())
+        bot = TelegramBot(file.read(), return_on_update_only=False)
 
     bot.deleteBotCommands()
     bot.setBotCommands(BotCommandList([BotCommand("sim", "Simulation einer Aktion")]))
 
-    userlist = UserIdList()
-    print (userlist)
+    userlist = UserIdList(time_interval_=5)
 
     while True:
+
+        for user in userlist.timeUp():
+            print ("kicked a user after 5 seconds")
+            bot.kickChatMember(user[0], user[1])
+            userlist.unregister(user[0], user[1])
+
         update = bot.poll()
+        if not update:
+            sleep(1)
+            continue
 
         # --- simulation requirements ---
         command_entity = update.isBotCommand()
@@ -34,34 +46,39 @@ if __name__ == '__main__':
         if command_entity:
             command = update.message.text[command_entity.offset:command_entity.length]
             if "/sim" in command:
-
                 # simulate different states of script
-
                 command_params = update.message.text[command_entity.offset + command_entity.length:].strip()
-                if command_params == "join":
+
+                if "join" in command_params:
                     # simulate first join in group
-                    print ("Triggered Simulation: On first join")
+                    # register user-id
+
+                    user_param = command_params.replace("join", "")
+                    user_param = user_param.replace(" ", "")
+
+                    newChatMember(update.message.chat.id, user_param, update.message.date, userlist)
                     continue
                 
                 if command_params == "reg":
                     # simlate registering of user
                     userlist.register(update.message.chat.id, update.message.sender.id, update.message.date)
-                    print(userlist)
-                    continue
                 
                 if command_params == "unreg":
                     # simlate unregistering of user
                     userlist.unregister(update.message.chat.id, update.message.sender.id)
-                    print(userlist)
-                    continue
-        
+
+
         # --- real life action ---
 
-        if update.isnewChatMember():
-            # start captcha procedure
+        if update.isMessage():
+            print (f"{update.message.text} from {update.message.sender.id} in {update.message.chat.id}")
 
-            # onNewChatMember()
-            pass
+
+        if update.isnewChatMember():
+            # print("new chat member")
+            # userlist.register(update.message.chat.id, update.message.sender.id, update.message.date)
+            newChatMember (update.message.chat.id, update.message.sender.id, update.message.date, userlist)
+        
         # check frequently to not overuse capacitites
         sleep(1)
 
