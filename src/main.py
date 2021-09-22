ea4cf6631c0ed672b66e6a45b598ea7ca8c894ea
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-09-22 13:31:42 +0200

    message and keyboard for new members

--- src/main.py
+++ src/main.py
@@ -4,17 +4,12 @@ from time import sleep, time
 
 import path_setup as setup
 
-from telegram_bot import TelegramBot, BotCommand, BotCommandList
+from telegram_bot import TelegramBot, BotCommand, BotCommandList, InlineButton, ButtonList
 
 # from lib.events import onNewChatMember
 from classes.user_id_list import UserIdList
 
 if __name__ == '__main__':
-
-    def newChatMember(chat, user, time, userlist):
-        print (f"New chat member {user} in {chat} at {time}")
-        userlist.register(chat, user, time)
-
     setup.enable()
 
     with open('bot_credentials/chat_id.txt', 'r') as file:
@@ -23,16 +18,24 @@ if __name__ == '__main__':
     with open('bot_credentials/token.txt', 'r') as file:
         bot = TelegramBot(file.read(), return_on_update_only=False)
 
+    
+    def newChatMember(chat, user, time, userlist):
+        print (f"New chat member {user} in {chat} at {time}")
+        userlist.register(chat, user, time)
+        welcome_message = f"Willkommen im Chat {user}!. Bitte drücke auf den untenstehenden Knopf um der Konversation beitreten zu können:"
+        button_dict = ButtonList (InlineButton, [InlineButton("Der Konversation beitreten", "join_button", f"https://t.me/johannes_group_mod_bot?start={chat}")]).toBotDict()
+        bot.sendMessage(chat, welcome_message, button_dict)
+
     bot.deleteBotCommands()
     bot.setBotCommands(BotCommandList([BotCommand("sim", "Simulation einer Aktion")]))
 
-    userlist = UserIdList(time_interval_=5)
+    userlist = UserIdList(time_interval_=10)
 
     while True:
 
         for user in userlist.timeUp():
-            print ("kicked a user after 5 seconds")
-            bot.kickChatMember(user[0], user[1])
+            print (f"kicked user {user[1]} in {user[0]}")
+            print (bot.kickChatMember(user[0], user[1]))
             userlist.unregister(user[0], user[1])
 
         update = bot.poll()
@@ -45,9 +48,20 @@ if __name__ == '__main__':
 
         if command_entity:
             command = update.message.text[command_entity.offset:command_entity.length]
+            command_params = update.message.text[command_entity.offset + command_entity.length:].strip()
+            if "/start" in command:
+                # a user has started conversation with bot
+                # if conversation initianted from a group, then chat-id of group will be payload
+                payload = command_params.replace(" ", "")
+
+                if payload in userlist:
+                    bot.sendMessage(update.message.chat.id, "Willkommen in der Gang!")
+                    userlist.unregister (payload, update.message.sender.id)
+                
+                continue
+
             if "/sim" in command:
                 # simulate different states of script
-                command_params = update.message.text[command_entity.offset + command_entity.length:].strip()
 
                 if "join" in command_params:
                     # simulate first join in group
@@ -56,8 +70,7 @@ if __name__ == '__main__':
                     user_param = command_params.replace("join", "")
                     user_param = user_param.replace(" ", "")
 
-                    newChatMember(update.message.chat.id, user_param, update.message.date, userlist)
-                    continue
+                    newChatMember(update.message.chat.id, user_param if user_param else update.message.sender.id, update.message.date, userlist)
                 
                 if command_params == "reg":
                     # simlate registering of user
@@ -67,17 +80,20 @@ if __name__ == '__main__':
                     # simlate unregistering of user
                     userlist.unregister(update.message.chat.id, update.message.sender.id)
 
+                continue
 
         # --- real life action ---
 
         if update.isMessage():
             print (f"{update.message.text} from {update.message.sender.id} in {update.message.chat.id}")
-
+            # print (f"{update.message.text} from {update.message.sender.id} in {update.message.chat.id}:\n{update.content}")
+            pass
 
         if update.isnewChatMember():
             # print("new chat member")
             # userlist.register(update.message.chat.id, update.message.sender.id, update.message.date)
-            newChatMember (update.message.chat.id, update.message.sender.id, update.message.date, userlist)
+            for new_member in update.message.new_chat_members:
+                newChatMember (update.message.chat.id, new_member.id, update.message.date, userlist)
         
         # check frequently to not overuse capacitites
         sleep(1)
