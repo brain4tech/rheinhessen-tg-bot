54c28626af70a489f2f20381a29f66796013a648
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-09-27 21:50:10 +0200

    moved debug_print to own file

---
+++ src/lib/misc.py
@@ -0,0 +1,6 @@
+# miscellanious functions for cleaning up main script
+
+def debug_print(message, debug = False):
+    """prints a given statement to console if debug equals True"""
+    if debug:
+        print (message)
