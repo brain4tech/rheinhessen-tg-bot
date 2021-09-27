e1ec0399ca57e5fbd7090eae649575b1cbfe2088
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-09-27 21:49:21 +0200

    restructured class into baseclass for inheritance

--- src/classes/user_id_list.py
+++ src/classes/user_id_list.py
@@ -1,54 +1,55 @@
 
 import json
 from time import time
+import os
 
 class UserIdList:
-    """A list (dict) of user-id's sorted after the chat-id"""
-    def __init__(self, save_path_: str = "", time_interval_ = 30):
-        self.__id_list = {}
-        self.__save_path = "data/id_list.json"
-        self.__time_interval = time_interval_
+    """A datastructure (dict) of user-id specific data structured with the chat-id"""
+    def __init__(self, file_name_):
+        self._id_list = {}
+        # TODO: create paths if not existent
 
-        if save_path_:
-            self.__save_path = save_path_        
+        os.makedirs("data", exist_ok=True)
+        self._file_name = os.path.join("data", file_name_)
+        print (self._file_name)        
 
         try:
-            with open(self.__save_path, "r") as file:
-                self.__id_list = json.loads(file.read())
+            with open(self._file_name, "r") as file:
+                self._id_list = json.loads(file.read())
         except Exception:
             pass
 
     def __call__(self):
-        return self.__id_list
+        return self._id_list
     
     def __contains__(self, key):
-        return key in self.__id_list
+        return key in self._id_list
     
     def __str__(self):
-        return str(self.__id_list)
+        return str(self._id_list)
     
-    def save(self, save_path_: str = ""):
+    def save(self, file_name_: str = ""):
         """safe the id_list"""
-        if save_path_:
-            with open(save_path_, "w") as file:
-                file.write(json.dumps(self.__id_list))
+        if file_name_:
+            with open(file_name_, "w") as file:
+                file.write(json.dumps(self._id_list))
 
-        elif self.__save_path:
-            with open(self.__save_path, "w") as file:
-                file.write(json.dumps(self.__id_list))
+        elif self._file_name:
+            with open(self._file_name, "w") as file:
+                file.write(json.dumps(self._id_list))
 
     
-    def register(self, chat_id, user_id, user_timestamp):
+    def register(self, chat_id, user_id, user_data):
         """registers a new user in a group"""
 
         chat_id = str(chat_id)
         user_id = str(user_id)
 
-        if chat_id in self.__id_list:
-            self.__id_list[chat_id][user_id] = user_timestamp
+        if chat_id in self._id_list:
+            self._id_list[chat_id][user_id] = user_data
         else:
-            self.__id_list[chat_id] = {}
-            self.__id_list[chat_id][user_id] = user_timestamp
+            self._id_list[chat_id] = {}
+            self._id_list[chat_id][user_id] = user_data
         
         self.save()
 
@@ -58,25 +59,39 @@ class UserIdList:
         chat_id = str(chat_id)
         user_id = str(user_id)
 
-        if chat_id in self.__id_list:
-            self.__id_list[chat_id].pop(user_id, None)
+        if chat_id in self._id_list:
+            self._id_list[chat_id].pop(user_id, None)
         
-            if not self.__id_list[chat_id]:
-                self.__id_list.pop(chat_id, None)
+            if not self._id_list[chat_id]:
+                self._id_list.pop(chat_id, None)
         
         self.save()
     
-    def timeUp(self, custom_interval = None):
+    def getList(self, chat_id = None):
+        if chat_id:
+            try:
+                return self._id_list[chat_id]
+            except Exception:
+                return None
+        
+        return self._id_list
+
+class UserIdTimestampList(UserIdList):
+    def __init__(self, file_name_, time_interval_ = None):
+        super().__init__(file_name_)
+        self.__time_interval = time_interval_
+    
+
+    def getExpiredUsers(self, custom_interval = None):
         timeup_list = []
         
         timestamp = int(time())
 
         interval = custom_interval if custom_interval else self.__time_interval
 
-        for chat in self.__id_list:
-            for user in self.__id_list[chat]:
-                if timestamp - self.__id_list[chat][user] >= interval:
+        for chat in self._id_list:
+            for user in self._id_list[chat]:
+                if timestamp - self._id_list[chat][user] >= interval:
                     timeup_list.append([chat, user])
         
         return timeup_list
-
