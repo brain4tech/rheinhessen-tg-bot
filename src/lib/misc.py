03ab0aadc21f3951385fdd947f4dac1b5940bf72
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-09-29 20:36:33 +0200

    made debug a neccessary parameter

--- src/lib/misc.py
+++ src/lib/misc.py
@@ -1,6 +1,6 @@
 # miscellanious functions for cleaning up main script
 
-def debug_print(message, debug = False):
+def debug_print(message, debug):
     """prints a given statement to console if debug equals True"""
     if debug:
         print (message)
