accdf3602c1e5a7044594c9ccb95c1a54c6da134
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-09-29 11:19:56 +0200

    implemented userwelcomelist

--- src/main.py
+++ src/main.py
@@ -21,13 +21,15 @@ DEBUG = True
 
 # --- FUNCTIONS ---
 
-def newChatMember(chat_id, user_id, user_name, time, userlist):
+def newChatMember(chat_id, user_id, user_name, time):
     debug_print (f"New chat member {user_id} in {chat_id} at {time}", DEBUG)
     # TODO: restrict user
-    userlist.register(chat_id, user_id, time)
+    usertimestamplist.register(chat_id, user_id, time)
     welcome_message = f"Willkommen im Chat {user_name}! Bitte drücke auf den untenstehenden Knopf um der Konversation beitreten zu können:"
     button_dict = ButtonList (InlineButton, [InlineButton("Der Konversation beitreten", "join_button", f"https://t.me/rheinhessen_test_group_bot?start={chat_id}")]).toBotDict()
-    bot.sendMessage(chat_id, welcome_message, button_dict)
+    response = bot.sendMessage(chat_id, welcome_message, button_dict)
+    userwelcomemessagelist.register(chat_id, user_id, response['result']['message_id'])
+    return response
 
 def createNeededFileStructure():
     # credentials
@@ -52,26 +54,28 @@ with open('bot_credentials/chat_id.txt', 'r') as file:
     chat_id = file.read()
 
 with open('bot_credentials/token.txt', 'r') as file:
-    token = file.read()
-    if token:
-        bot = TelegramBot(file.read(), return_on_update_only=False)
-    else:
-        raise ValueError("No token found in file")
-
+    bot = TelegramBot(file.read(), return_on_update_only=False)
+    
 bot.deleteBotCommands()
 bot.setBotCommands(BotCommandList([BotCommand("sim", "Simulation einer Aktion [event] [parameter]")]))
 
-userlist = UserIdTimestampList("user_id_timestamp.json", time_interval_=10)
+# create lists
+usertimestamplist = UserIdTimestampList("user_id_timestamp.json", time_interval_=10)
+userwelcomemessagelist = UserIdList("user_welcome_message.json")
 
 print ("--- Started Rheinhessen TelegramBot ---")
 
 while True:
 
-    for user in userlist.getExpiredUsers():
-        response_ban = bot.kickChatMember(user[0], user[1])
-        debug_print (f"kicked user {user[1]} in {user[0]}: {response_ban}", DEBUG)
-        userlist.unregister(user[0], user[1])
-        # TODO: delete welcome message in group
+    for user in usertimestamplist.getExpiredUsers():
+        # kick user from chat
+        response_ban, _ = bot.kickChatMember(user[0], user[1])
+        debug_print (f"kicked user {user[1]} in {user[0]}: [{response_ban[list(response_ban)[-1]]}]", DEBUG)
+        usertimestamplist.unregister(user[0], user[1])
+
+        # delete welcome_message
+        bot.deleteMessage(user[0], userwelcomemessagelist.getList()[user[0]][user[1]])
+        userwelcomemessagelist.unregister(user[0], user[1])
 
     update, response = bot.poll()
     # debug_print(response)
@@ -79,7 +83,6 @@ while True:
         sleep(1)
         continue
 
-    # --- SIMULATION OF GROUP-JOINING ---
     command_entity = update.isBotCommand()
 
     if command_entity:
@@ -91,12 +94,15 @@ while True:
         if "/start" in command:
             payload = command_params.replace(" ", "")
 
-            if payload in userlist:
+            if payload in usertimestamplist:
                 bot.sendMessage(update.message.chat.id, "Willkommen in der Gang!")
-                userlist.unregister (payload, update.message.sender.id)
+                usertimestamplist.unregister (payload, update.message.sender.id)
+                welcome_string = f"Willkommen in der Gruppe {update.message.sender.first_name}!"
+                bot.editMessage(payload, userwelcomemessagelist.getList[payload][update.message.sender.id], welcome_string)
+                userwelcomemessagelist.unregister (payload, userwelcomemessagelist.getList[payload][update.message.sender.id])
                 # TODO: update user permissions
-                # TODO: update previous message to other message
 
+        # --- SIMULATION OF GROUP-JOINING ---
         if "/sim" in command:
             # simulate different states of script
 
@@ -107,17 +113,17 @@ while True:
 
                 #TODO: get user_name from group if user_param is given
 
-                newChatMember(update.message.chat.id, user_id if user_id else update.message.sender.id, update.message.sender.first_name, update.message.date, userlist)
+                newChatMember(update.message.chat.id, user_id if user_id else update.message.sender.id, bot.getChatMember(update.message.chat.id, user_id if user_id else update.message.sender.id).user.first_name, update.message.date)
             
             """
             # simulation registration is not neccessary as it is integrated in "/sim join"
             if command_params == "reg":
                 # simlate registering of user
-                userlist.register(update.message.chat.id, update.message.sender.id, update.message.date)
+                usertimestamplist.register(update.message.chat.id, update.message.sender.id, update.message.date)
             
             if command_params == "unreg":
                 # simlate unregistering of user
-                userlist.unregister(update.message.chat.id, update.message.sender.id)
+                usertimestamplist.unregister(update.message.chat.id, update.message.sender.id)
             """
 
         continue
@@ -133,14 +139,23 @@ while True:
 
     if update.isnewChatMember():
         for new_member in update.message.new_chat_members:
-            newChatMember (update.message.chat.id, new_member.id, update.message.date, userlist)
+            newChatMember (update.message.chat.id, new_member.id, update.message.date, usertimestamplist)
     
     # security issue: check if correct user has pressed correct button for his message
     # temporarily store user with connected message-id and check with:
 
     if update.isCallback():
         # TODO: see above
-        pass
+        callback_id = update.callback.id
+        callback_message_id = update.callback.message.id
+        callback_chat_id = update.callback.message.chat.id
+        callback_user_id = update.callback.sender.id
+
+        if callback_user_id in usertimestamplist.getList()[callback_chat_id]:
+            if userwelcomemessagelist.getList()[callback_chat_id][callback_user_id] != callback_message_id:
+                bot.answerCallbackQuery(callback_id, "Bitte drücke auf den Knopf unter deiner eigenen Willkommensnachricht")
+        else:
+            bot.answerCallbackQuery(callback_id, "Du brauchst diesen Knopf nicht zu drücken da du schon in der Gruppe bist!")
 
     # check frequently to not overuse capacities
     sleep(1)
