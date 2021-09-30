0a1e48d4516a187fcc8f8fbb324a4233ab653830
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-09-30 15:34:12 +0200

    use latest bot-package version (1.2.2)

--- src/main.py
+++ src/main.py
@@ -78,8 +78,8 @@ while True:
 
     for user in usertimestamplist.getExpiredUsers():
         # kick user from chat
-        response_kick = bot.kickChatMember(user[0], user[1]).json()
-        debug_print (f"kicked user {user[1]} in {user[0]}: [{response_kick[list(response_kick)[-1]]}]", DEBUG)
+        response_ban, _ = bot.kickChatMember(user[0], user[1])
+        debug_print (f"kicked user {user[1]} in {user[0]}: [{response_ban.json()[list(response_ban.json())[-1]]}]", DEBUG)
         usertimestamplist.unregister(user[0], user[1])
 
         # delete welcome_message
